3c902678688faa6902c23d2320ef3aca3fd67532
Jett Chen <jettchen12345@gmail.com>
2023-12-10 02:03:53 +0800

    Add cleanup to script

--- bench/run_all.sh
+++ bench/run_all.sh
@@ -1,5 +1,7 @@
 # Define the number of servers
-SRV_LEN=1
+SRV_LEN=$(ls -1 config/cluster_res/ | wc -l)
+
+echo "Starting ${SRV_LEN} servers..."
 
 # Function to run server commands
 run_server() {
@@ -7,6 +9,15 @@ run_server() {
     RUST_BACKTRACE=1 RUST_LOG=debug target/debug/MerkleVerseWrapper server -c config/cluster_res/srv_${i}.toml &
     PORT=$((6000+i)) ../MerkleSquare/demo/mverserver/mverserver &
 }
+
+cleanup() {
+    echo "Interrupt received, stopping servers..."
+    kill $(jobs -p)  # Kills all jobs running in the background
+    exit 0
+}
+
+# Trap SIGINT (Ctrl+C)
+trap cleanup SIGINT
 
 # Start all servers
 for ((i=0; i<SRV_LEN; i++)); do
